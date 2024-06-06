116911a8aa42e8a0bf270a4b360379866d35119c
Huai-mmlab <huai0429.gcp@gmail.com>
2024-06-06 19:12:47 +0800

    First Commmit of FreeRTOS

---
+++ Core/Src/freertos.c
@@ -0,0 +1,215 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include<stdio.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for Send */
+osThreadId_t SendHandle;
+const osThreadAttr_t Send_attributes = {
+  .name = "Send",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for Receive */
+osThreadId_t ReceiveHandle;
+const osThreadAttr_t Receive_attributes = {
+  .name = "Receive",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow2,
+};
+/* Definitions for Sending */
+osEventFlagsId_t SendingHandle;
+const osEventFlagsAttr_t Sending_attributes = {
+  .name = "Sending"
+};
+/* Definitions for Receiving */
+osEventFlagsId_t ReceivingHandle;
+const osEventFlagsAttr_t Receiving_attributes = {
+  .name = "Receiving"
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+void SendTask(void *argument);
+void ReceiveTask(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of Send */
+  SendHandle = osThreadNew(SendTask, NULL, &Send_attributes);
+
+  /* creation of Receive */
+  ReceiveHandle = osThreadNew(ReceiveTask, NULL, &Receive_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* Create the event(s) */
+  /* creation of Sending */
+  SendingHandle = osEventFlagsNew(&Sending_attributes);
+
+  /* creation of Receiving */
+  ReceivingHandle = osEventFlagsNew(&Receiving_attributes);
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    printf("Default Task %d\n",defaultTask_attributes.priority);
+    osDelay(1);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_SendTask */
+/**
+* @brief Function implementing the Send thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_SendTask */
+void SendTask(void *argument)
+{
+  /* USER CODE BEGIN SendTask */
+  /* Infinite loop */
+  int sendflag = 1;
+  for(;;)
+  {
+    printf("Send Task %d %d\n",Send_attributes.priority, sendflag);
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_6,sendflag);
+    sendflag = !sendflag;
+    osDelay(10);
+  }
+  /* USER CODE END SendTask */
+}
+
+/* USER CODE BEGIN Header_ReceiveTask */
+/**
+* @brief Function implementing the Receive thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_ReceiveTask */
+void ReceiveTask(void *argument)
+{
+  /* USER CODE BEGIN ReceiveTask */
+  /* Infinite loop */
+  // char *task_data;
+  // task_data = (char*) argument;
+  int recflag = 1;
+  for(;;)
+  {
+    printf("Receive Task %d %d\n",Receive_attributes.priority,recflag);
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,recflag);
+    recflag = !recflag;
+    osDelay(100);
+  }
+  /* USER CODE END ReceiveTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
